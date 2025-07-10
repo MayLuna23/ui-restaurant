61c2873e9cfe8fca5d0a8d7fa63db01e27f91ac2
Mayra Luna <mayraluna9723@gmail.com>
2025-07-10 01:00:20 -0500

    Fix typo .env, ready to run in local

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,4 +3,4 @@ const VITE_ENV: string = import.meta.env.VITE_ENV;
 const VITE_DEV_API_URL: string = import.meta.env.VITE_DEV_API_URL;
 const VITE_PROD_API_URL: string = import.meta.env.VITE_PROD_API_URL
 
-export const API_URL = VITE_ENV === 'development' ? `http://${VITE_DEV_API_URL}` : `${VITE_PROD_API_URL}`;
+export const API_URL = VITE_ENV === 'development' ? `${VITE_DEV_API_URL}` : `${VITE_PROD_API_URL}`;
